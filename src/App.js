5e45019c0638ff9213196237f8fc5f30b487b187
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2021-02-23 13:23:33 +0000

    Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
 
 function App() {
-  return <div className="App"></div>;
+  //BEM
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      <Home />
+
+      {/* Banner */}
+
+      {/* Cards */}
+
+      {/* Footer */}
+
+      {/* SEARCH */}
+    </div>
+  );
 }
 
 export default App;
