d854e50dafe23cac8bc40f09be4d40d5e432079c
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2021-02-23 15:46:21 +0000

    Updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import React from "react";
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
+import SearchPage from "./SearchPage";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  //BEM
   return (
+    // BEM
     <div className="app">
-      {/* Header */}
-      <Header />
-      <Home />
+      <Router>
+        <Header />
 
-      {/* Banner */}
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-      {/* Cards */}
-
-      {/* Footer */}
-
-      {/* SEARCH */}
+        <Footer />
+      </Router>
     </div>
   );
 }
